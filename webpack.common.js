248b8df8ec9050d281564e61e673ff49fd30bfdd
DantsevichIvan <iw.dantsevich@gmail.com>
2020-12-03 14:29:58 +0300

    feat: update webpack.prod.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const {CleanWebpackPlugin} =require('clean-webpack-plugin')
 const HTMLWebpackPlugin =require('html-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin");
+
 
 
 module.exports = {
@@ -15,10 +15,7 @@ module.exports = {
         new HTMLWebpackPlugin({template: "./client/index.html"}),
         new CleanWebpackPlugin()
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    },
+
     module: {
         rules: [
             {
