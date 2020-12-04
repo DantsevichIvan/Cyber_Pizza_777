dfe881b4d518515c7a6cd52dfe864a895255c69f
DantsevichIvan <iw.dantsevich@gmail.com>
2020-12-04 20:33:36 +0300

    feat: add FormUpdateProduct.js

--- client/component/FormAddProduct.js
+++ client/component/FormAddProduct.js
@@ -102,8 +102,6 @@ const FormAddProduct = ({closeModal, handleSubmit}) => {
 
             )}
         </Formik>
-
-
     );
 };
 
---
+++ client/component/FormUpdateProduct.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+import {Field, Formik} from "formik";
+
+const FormUpdateProduct = ({closeModal,handleSubmit,product}) => {
+    const categories = useSelector(state => state.categories.categories)
+    return (
+        <Formik
+            initialValues={{
+                name: product.name,
+                description: product.description,
+                weight: product.weight,
+                price: product.price,
+                categories: product.categories,
+                image: product.image
+            }}
+            onSubmit={handleSubmit}
+        >
+            {({
+                  values,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+              }) => (
+                <form action="" className='form' onSubmit={handleSubmit}>
+                    <div className='form-header'>
+                        <h3>Add new Product</h3>
+                        <button onClick={closeModal} className='btn-close'>Close</button>
+                    </div>
+                    <div className='form-wrap'>
+                        <div className='form-item'>
+                            <label htmlFor="">Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                            />
+                        </div>
+                        <div className='form-item'>
+                            <label htmlFor="">Description</label>
+                            <textarea
+                                name='description'
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.description}/>
+                        </div>
+                        <div className='form-item'>
+                            <label htmlFor="">Weight</label>
+                            <input
+                                type="number"
+                                name="weight"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.weight}
+                            />
+                        </div>
+                        <div className='form-item'>
+                            <label htmlFor="">Price</label>
+                            <input
+                                type="number"
+                                name="price"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.price}
+                            />
+                        </div>
+                        <div className='form-item'>
+                            <label htmlFor="">Image</label>
+                            <input
+                                type="text"
+                                name="image"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.image}
+                            />
+                            <a href="https://dantsevichivan.github.io/Search-Image/" target='_blank'>Image Search</a>
+                        </div>
+                        <div className='form-item'>
+                            <label htmlFor="">Categories</label>
+                            <Field name="categories" as="select" >
+                                {
+                                    categories.map((item) => {
+                                        return<option
+                                            key={item._id}
+                                            value={item.name}>
+                                            {item.name}
+                                        </option>
+                                    })
+                                }
+                            </Field>
+                        </div>
+                    </div>
+                    <div className='btn-add'>
+                        <button type="submit" disabled={isSubmitting}>Create Product</button>
+                    </div>
+                </form>
+
+            )}
+        </Formik>
+    );
+};
+
+export default FormUpdateProduct;
